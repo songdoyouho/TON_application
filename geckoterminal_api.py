4ea69dbb1c624ac2268d059359eacd0b50aee1b4
Your Name <songdoyouho@gmail.com>
2024-05-13 21:45:56 +0800

    定時查詢新 pool 並記錄

--- geckoterminal_api.py
+++ geckoterminal_api.py
@@ -1,6 +1,7 @@
-import requests
 import time
 import json
+import datetime
+import requests
 
 # 設定 API URL 和您的 API Key
 API_URL = "https://api.geckoterminal.com/api/v2"
@@ -81,15 +82,31 @@ class Geckoterminal_api():
 
 if __name__ == "__main__":
     geckoterminal_api = Geckoterminal_api()
-    print(json.dumps(geckoterminal_api.get_latest_pool_on_network('ton')['data'][0], indent=4))
-    print(len(geckoterminal_api.get_latest_pool_on_network('ton')['data']))
+    # print(json.dumps(geckoterminal_api.get_latest_pool_on_network('ton')['data'][0], indent=4))
+    # print(len(geckoterminal_api.get_latest_pool_on_network('ton')['data']))
 
-    latest_pools = geckoterminal_api.get_latest_pool_on_network('ton')
-    for pool in latest_pools['data']:
-        # print(json.dumps(pool, indent=4))
-        print(pool['attributes']['name'])
-        # print(pool['attributes']['pool_created_at']) # 時間是 +0，換算過來要再 +8
+    # latest_pools = geckoterminal_api.get_latest_pool_on_network('ton')
+    # for pool in latest_pools['data']:
+    #     # print(json.dumps(pool, indent=4))
+    #     print(pool['attributes']['name'])
+    #     # print(pool['attributes']['pool_created_at']) # 時間是 +0，換算過來要再 +8
 
-        # 拿 pool_address
-        result = geckoterminal_api.get_specific_pool_on_network('ton', pool['id'].split('ton_')[1])
-        print(result)
+    #     # 拿 pool_address 得到 pool 資訊0
+    #     result = geckoterminal_api.get_specific_pool_on_network('ton', pool['id'].split('ton_')[1])
+    #     print(result)
+
+    new_pool_list = []
+
+    while True:
+        current_time = time.localtime()
+        if current_time.tm_min % 1 == 0 and current_time.tm_sec == 0: # 每?分鐘執行一次
+            latest_pools = geckoterminal_api.get_latest_pool_on_network('ton')
+            for pool in latest_pools['data']:
+                print(json.dumps(pool, indent=4))
+                if pool not in new_pool_list:
+                    new_pool_list.append(pool)
+
+        time.sleep(0.5)
+
+        if current_time.tm_min % 1 == 0 and current_time.tm_sec == 30:
+            print(len(new_pool_list))
