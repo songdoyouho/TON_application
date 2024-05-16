2ae3e87e330f7b345aaff8b00535c22205da88e3
tengkai <tengkai.yu@deltaww.com>
2024-05-16 16:26:00 +0800

    新增傳送 telegram 訊息及檢查買進數量回報功能

--- geckoterminal_api.py
+++ geckoterminal_api.py
@@ -4,6 +4,7 @@ import datetime
 import requests
 from datetime import datetime
 import pytz
+import config
 
 # 設定 API URL 和您的 API Key
 API_URL = "https://api.geckoterminal.com/api/v2"
@@ -19,6 +20,46 @@ def get_local_time(iso_time_str):
     local_time = utc_time.replace(tzinfo=pytz.utc).astimezone(local_tz)
 
     return local_time
+
+def telegram_bot_sendtext(bot_message):
+    bot_token = config.TELEGRAM_API
+    MY_TELEGRAM_ID = config.MY_TELEGRAM_ID
+
+    # 送訊息給我
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + MY_TELEGRAM_ID + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+
+    return response.json()
+
+def check_buy_number(pool):
+    output_list = []
+
+    if pool['attributes']['transactions']['m5']['buys'] > 10:
+        print('m5 large buy ' + pool['attributes']['name'])
+        # telegram_bot_sendtext('m5 large buy ' + pool['attributes']['name'])
+        output_list.append('m5 large buy ' + pool['attributes']['name'])
+
+    if pool['attributes']['transactions']['m15']['buys'] > 25:
+        print('m15 large buy ' + pool['attributes']['name'])
+        # telegram_bot_sendtext('m15 large buy ' + pool['attributes']['name'])
+        output_list.append('m15 large buy ' + pool['attributes']['name'])
+
+    if pool['attributes']['transactions']['m30']['buys'] > 50:
+        print('m30 large buy ' + pool['attributes']['name'])
+        # telegram_bot_sendtext('m30 large buy ' + pool['attributes']['name'])
+        output_list.append('m30 large buy ' + pool['attributes']['name'])
+
+    if pool['attributes']['transactions']['h1']['buys'] > 100:
+        print('h1 large buy ' + pool['attributes']['name'])
+        # telegram_bot_sendtext('h1 large buy ' + pool['attributes']['name'])
+        output_list.append('h1 large buy ' + pool['attributes']['name'])
+
+    if pool['attributes']['transactions']['h24']['buys'] > 500:
+        print('h24 large buy ' + pool['attributes']['name'])
+        # telegram_bot_sendtext('h24 large buy ' + pool['attributes']['name'])
+        output_list.append('h24 large buy ' + pool['attributes']['name'])
+
+    return output_list
 
 class Geckoterminal_api():
     def get_current_price_of_a_token(self, network, token_address):
@@ -73,7 +114,7 @@ class Geckoterminal_api():
     def get_latest_pool_on_network(self, network):
         """
         """
-        url = f"{API_URL}/networks/{network}/new_pools?page=1"
+        url = f"{API_URL}/networks/{network}/new_pools?page=5"
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
@@ -96,6 +137,23 @@ class Geckoterminal_api():
 
 if __name__ == "__main__":
     geckoterminal_api = Geckoterminal_api()
+    telegram_bot_sendtext('scanning the new pools...')
+    output_list = []
+
+    while True:
+        current_time = time.localtime()
+        if current_time.tm_min % 1 == 0 and current_time.tm_sec == 0: # 每?分鐘執行一次
+            latest_pools = geckoterminal_api.get_latest_pool_on_network('ton')
+            for pool in latest_pools['data']:
+                # print(json.dumps(pool, indent=4))
+                tmp_output_list = check_buy_number(pool)
+                for tmp_output in tmp_output_list:
+                    if tmp_output not in output_list:
+                        output_list.append(tmp_output)
+                        telegram_bot_sendtext(tmp_output)
+
+        time.sleep(1)
+
     # print(json.dumps(geckoterminal_api.get_latest_pool_on_network('ton')['data'][0], indent=4))
     # print(len(geckoterminal_api.get_latest_pool_on_network('ton')['data']))
 
@@ -108,22 +166,3 @@ if __name__ == "__main__":
     #     # 拿 pool_address 得到 pool 資訊0
     #     result = geckoterminal_api.get_specific_pool_on_network('ton', pool['id'].split('ton_')[1])
     #     print(result)
-
-    new_pool_list = []
-
-    while True:
-        current_time = time.localtime()
-        if current_time.tm_min % 1 == 0 and current_time.tm_sec == 0: # 每?分鐘執行一次
-            latest_pools = geckoterminal_api.get_latest_pool_on_network('ton')
-            for pool in latest_pools['data']:
-                # print(json.dumps(pool, indent=4))
-                if pool not in new_pool_list:
-                    # pool['attributes']['pool_created_at'] = get_local_time(pool['attributes']['pool_created_at'])
-                    new_pool_list.append(pool)
-
-        time.sleep(1)
-
-        if current_time.tm_min % 1 == 0 and current_time.tm_sec == 30:
-            print(len(new_pool_list))
-            for pool in new_pool_list:
-                print(json.dumps(pool, indent=4))
--- ton_api.py
+++ ton_api.py
@@ -11,17 +11,33 @@ def get_jettons(account_id, currencies):
         return response.json()
     else:
         return {"error": f"Failed to fetch data, status code: {response.status_code}"}
+    
+def get_account_transactions(account_id):
+    url = f"https://tonapi.io/v2/blockchain/accounts/{account_id}/transactions"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": f"Failed to fetch data, status code: {response.status_code}"}
+
 
 if __name__ == "__main__":
-    account_id = "UQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJKZ"
+    # account_id = "UQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJKZ"
     # account_id = "UQB7RQglXjFhNlIgGaAD-WDmrulKGXhH6HfKU_M4YPKWaxeZ"
-    currencies = ["usd", "ton"]
-    
-    data = get_jettons(account_id, currencies)
-    
-    # print(json.dumps(data, indent=4))
-    print(len(data['balances']))
 
-    for balance in data['balances']:
-        if 'LP' in balance['jetton']['symbol']:
-            print(json.dumps(balance['jetton'], indent=4))
+    # account_id = "EQCN8TpPuQud6STd34j0R4DYySdr56KUO2SBnCxv7pdXwdJH"
+    # currencies = ["usd", "ton"]
+    
+    # data = get_jettons(account_id, currencies)
+    
+    # # print(json.dumps(data, indent=4))
+    # print(len(data['balances']))
+
+    # for balance in data['balances']:
+    #     if 'LP' in balance['jetton']['symbol']:
+    #         print(json.dumps(balance['jetton'], indent=4))
+
+    result = get_account_transactions("UQAKHdbLTBTYbgIeUzbcTuRnAiXkSwgHhiwzJfbKkowxjnXN")
+    print(json.dumps(result['transactions'][1]['out_msgs'][0]['decoded_body']['destination'], indent=4))
+    print(len(result['transactions'][1])) # 交易的排列順序是由新到舊
